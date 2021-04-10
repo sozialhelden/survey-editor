11232fca0ea007d0d7b3b6730c0b63cbb1fae900
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2021-04-10 20:35:17 +0200

    Improve JSON tree
    - Make nodes expandable/collapsable
    - Show JSON-LD class names
    - Improve dark mode readability

--- src/code/ResultCodeTree.tsx
+++ src/code/ResultCodeTree.tsx
@@ -1,20 +1,25 @@
 import { Colors, ControlGroup, Icon, ITreeNode, Tree } from "@blueprintjs/core";
 import { Tooltip2 } from "@blueprintjs/popover2";
 import * as React from "react";
+import { useDarkMode } from "../components/DarkModeContainer";
 import HighlightedExpression from "../components/HighlightedExpression/HighlightedODKExpression";
 import { ODKSurveyContext } from "../lib/ODKSurveyContext";
 import FieldPopoverButton from "../survey/FieldPopoverButton/FieldPopoverButton";
 import { XLSForm } from "../xlsform-simple-schema";
 import createLiteralExpressionFromValue from "../xlsform-simple-schema/functions/odk-formulas/evaluation/createLiteralExpressionFromValue";
+import { getJSONResult } from "../xlsform-simple-schema/functions/odk-formulas/evaluation/evaluateNodeAndChildren";
 import ODKFormulaEvaluationContext from "../xlsform-simple-schema/functions/odk-formulas/evaluation/ODKFormulaEvaluationContext";
-import { getNodeAbsolutePath } from "../xlsform-simple-schema/functions/odk-formulas/evaluation/XPath";
+import {
+  getNodeAbsolutePath,
+  getNodeAbsolutePathString,
+} from "../xlsform-simple-schema/functions/odk-formulas/evaluation/XPath";
 import {
   evaluatableColumnNames,
   ODKNode,
 } from "../xlsform-simple-schema/types/ODKNode";
 
 /** The label visible on the right of each tree item. */
-function SecondaryLabel(props: { node: ODKNode }) {
+function SecondaryLabel(props: { node: ODKNode; jsonResult: unknown }) {
   const { node } = props;
   const context = React.useContext(ODKSurveyContext);
   const results = context.evaluationContext?.evaluationResults.get(node);
@@ -59,6 +64,7 @@ function SecondaryLabel(props: { node: ODKNode }) {
 export function getNodeTree(
   node: ODKNode,
   context: ODKFormulaEvaluationContext,
+  jsonResult: unknown,
   transform: (result: ITreeNode<ODKNode>) => ITreeNode<ODKNode>
 ): ITreeNode<ODKNode> {
   if (node.children.length === 0) {
@@ -74,7 +80,7 @@ export function getNodeTree(
     id: getNodeAbsolutePath(node, context).join("."),
     label: node.row.name,
     childNodes: node.children.map((childNode) =>
-      getNodeTree(childNode, context, transform)
+      getNodeTree(childNode, context, jsonResult, transform)
     ),
     nodeData: node,
   });
@@ -85,44 +91,88 @@ export default function ResultCodeTree(props: { xlsForm: XLSForm }) {
   const { xlsForm } = props;
   const context = React.useContext(ODKSurveyContext);
   const evaluationContext = context.evaluationContext;
+  const [collapsedKeys, setCollapsedKeys] = React.useState<Set<string>>(
+    new Set()
+  );
+  const isDarkMode = useDarkMode();
+  const violetColor = isDarkMode ? Colors.VIOLET5 : Colors.VIOLET3;
 
   const result = React.useMemo(() => {
+    const jsonResult = evaluationContext?.survey
+      ? getJSONResult(evaluationContext?.survey, evaluationContext)
+      : {};
+
     return xlsForm && evaluationContext
-      ? getNodeTree(xlsForm.rootSurveyGroup, evaluationContext, (result) => {
-          const node = result.nodeData;
-          if (!node) {
-            debugger;
-            throw new Error(
-              "Encountered a tree node that is not associated with a node. Please fix this."
+      ? getNodeTree(
+          xlsForm.rootSurveyGroup,
+          evaluationContext,
+          jsonResult,
+          (result) => {
+            const node = result.nodeData;
+            if (!node) {
+              debugger;
+              throw new Error(
+                "Encountered a tree node that is not associated with a node. Please fix this."
+              );
+            }
+
+            if (!evaluationContext) {
+              throw new Error(
+                "No context defined. Please define a context before using this component."
+              );
+            }
+
+            const label = (
+              <FieldPopoverButton
+                node={node}
+                editable={true}
+                hasTypeIcon={true}
+                buttonStyle={{ color: violetColor }}
+              />
             );
+
+            return {
+              ...result,
+              label,
+              key: node.row.name,
+              isExpanded: !collapsedKeys.has(
+                getNodeAbsolutePathString(node, evaluationContext)
+              ),
+              hasCaret: !(result.childNodes?.length === 0),
+              secondaryLabel: node && (
+                <SecondaryLabel node={node} jsonResult={jsonResult} />
+              ),
+            } as ITreeNode<ODKNode>;
           }
-
-          if (!context.evaluationContext) {
-            throw new Error(
-              "No context defined. Please define a context before using this component."
-            );
-          }
-
-          const label = (
-            <FieldPopoverButton
-              node={node}
-              editable={true}
-              hasTypeIcon={true}
-              buttonStyle={{ color: Colors.VIOLET3 }}
-            />
-          );
-
-          return {
-            ...result,
-            label,
-            key: node.row.name,
-            isExpanded: true,
-            hasCaret: !(result.childNodes?.length === 0),
-            secondaryLabel: node && <SecondaryLabel node={node} />,
-          } as ITreeNode<ODKNode>;
-        })
+        )
       : null;
-  }, [context.evaluationContext, evaluationContext, xlsForm]);
+  }, [collapsedKeys, evaluationContext, xlsForm]);
 
-  return result ? <Tree contents={[result]} /> : <div></div>;
+  return result ? (
+    <Tree
+      onNodeExpand={(node) => {
+        if (!node.nodeData || !evaluationContext) {
+          return;
+        }
+        collapsedKeys.delete(
+          getNodeAbsolutePathString(node.nodeData, evaluationContext)
+        );
+        setCollapsedKeys(new Set(collapsedKeys));
+      }}
+      onNodeCollapse={(node) =>
+        node.nodeData &&
+        evaluationContext &&
+        setCollapsedKeys(
+          new Set(
+            collapsedKeys.add(
+              getNodeAbsolutePathString(node.nodeData, evaluationContext)
+            )
+          )
+        )
+      }
+      contents={[result]}
+    />
+  ) : (
+    <div></div>
+  );
 }
